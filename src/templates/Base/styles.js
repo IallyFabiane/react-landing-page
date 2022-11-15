f2801886b0d18b2d67a0fb21c8706d841fc8e4b7
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-11-14 22:23:46 -0300

    template Base parte 1

---
+++ src/templates/Base/styles.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  // eslint-disable-next-line no-unused-vars
+  ${({ theme }) => css``}
+`;
