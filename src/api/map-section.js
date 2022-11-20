08e7e335e9fec34cfe611dbb413e73b91331e18c
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-11-20 16:42:16 -0300

    mapeamento de dados parte 3

--- src/api/map-section.js
+++ src/api/map-section.js
@@ -9,7 +9,14 @@ export const mapSections = (sections = []) => {
     }
 
     if (section._component === 'section.section-grid') {
-      return mapSectionGrid(section);
+      const {__component: { text_grid = [], image_grid = []} = ''} = section
+
+      if (text_grid.length > 0) {
+        return mapTextGrid(section);
+      }
+      if (image_grid.length > 0) {
+        return mapImageGrid(section);
+       }
     }
 
     return section;
@@ -22,7 +29,7 @@ export const mapSectionTwoColumns = (section = {}) => {
     title = '',
     description: text = '',
     image: { url: srcImg = '' } = '',
-    metadata: { background = false, section_id: sectionId = ''} = false,
+    metadata: { background = false, section_id: sectionId = '' } = false,
   } = section;
   return {
     component,
@@ -34,10 +41,80 @@ export const mapSectionTwoColumns = (section = {}) => {
   };
 };
 
-export const mapSectionContent = (section) => {
-  return section;
+export const mapSectionContent = (section = {}) => {
+  const {
+    _component: component = '',
+    title = '',
+    // eslint-disable-next-line no-unused-vars
+    content: html = '',
+    metadata: { background = false, section_id: sectionId = '' } = false,
+  } = section;
+  return {
+    component,
+    title,
+    background,
+    sectionId,
+    html,
+  };
 };
 
-export const mapSectionGrid = (section) => {
-  return section;
+export const mapSectionTwoColumns = (section = {}) => {
+  const {
+    _component: component = '',
+    title = '',
+    description: text = '',
+    image: { url: srcImg = '' } = '',
+    metadata: { background = false, section_id: sectionId = '' } = false,
+  } = section;
+  return {
+    component,
+    title,
+    text,
+    srcImg,
+    background,
+    sectionId,
+  };
+};
+
+export const mapTextGrid = (section = {}) => {
+  const {
+    _component: component = '',
+    title = '',
+    content: html = '',
+    metadata: { background = false, section_id: sectionId = '' } = false,
+  } = section;
+  return {
+    component: 'section.section-grid-text',
+    title,
+    background,
+    sectionId,
+    html,
+  };
+};
+
+export const mapImageGrid = (section = {}) => {
+  const {
+    __component: component = '',
+    title = '',
+    description = '',
+    metadata: { background = false, section_id: sectionId = '' } = false,
+    image_grid: grid = [],
+  } = section;
+
+  return {
+    component: 'section.section-grid-image',
+    title,
+    background,
+    sectionId,
+    description,
+    grid: grid.map((img) => {
+      const {
+        image: { url: srcImg = '', alternativeText: altText = '' } = '',
+      } = img;
+      return {
+        srcImg,
+        altText,
+      };
+    }),
+  };
 };
