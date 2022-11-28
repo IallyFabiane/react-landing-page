f3322c17dc297c30daefadfcf6771b3ad638beb7
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-11-27 21:54:16 -0300

    adicionando rotas

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Home from './templates/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       <GlobalStyles />
-      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 );
